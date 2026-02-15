be1ee7d7ceebb63b3c58703484f6c8de3fd9a471
manoelvictorjs <manoelvictordejesus@outlook.com>
2026-02-15 12:22:06 -0300

    ajuste cv

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Isso permite que o build termine mesmo se o ESLint reclamar de versões
+    ignoreDuringBuilds: true, 
+  },
   images: {
     remotePatterns: [],
   },
